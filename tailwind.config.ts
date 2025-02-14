75abbfa7763785bb42b6b8aaeeb8aaabb7681b83
Cade Mcnally <cademcnally@Cades-MacBook-Air.local>
2025-02-14 14:39:21 -0700

    Initial front end commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontSize: {
+        'custom-size': '4.5rem', 
+      },
     },
   },
   plugins: [],
